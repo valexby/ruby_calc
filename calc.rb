9adb987841bc3eb9e07d7d6c09f28eacb1458a2e
Aliaksandr Vasiuk <aliaksandr_vasiuk@epam.com>
2017-08-23 16:39:00 +0300

    Working variant.

--- calc.rb
+++ calc.rb
@@ -1,26 +1,49 @@
-str = gets
-str.delete!(' ')
-str.delete!('_')
-str.delete!("\n")
+def proccess_strings(tokens)
+  tokens.map! do |token|
+    token != '+' && token != '<<' ? "\"#{token}\"" : token
+  end
+  tokens.reduce(:+)
+end
 
-str.gsub!(/(plus|add|minus|multiply|divide|point|dot)/,
-          'plus' => '+',
-          'add' => '+',
-          'minus' => '-',
-          'multiply' => '*',
-          'divide' => '/',
-          'point' => '.',
-          'dot' => '.')
-puts(str)
-p str.gsub!(/(-|\+|\*|\/)/) { |s| " #{s} " }
-tokens = str.split(' ')
-p tokens
+def calc(str)
+  str.delete!(' ')
+  str.delete!('_')
+  str.delete!("\n")
 
-is_complex = tokens.any? { |token| ['-', '*', '/'].include?(token) }
-is_number = tokens.all? { |token| token.match(/^(\d*\.?\d+)|[-\+\*\/]$/) }
-is_arrays = tokens.any? { |token| token.match(/^(\[(\d*\.?\d+)(,(\d*\.?\d+))*+\])|(\[\])$/) }
-is_arrays &&= tokens.all? { |token| token.match(/^(\[(\d*\.?\d+)(,(\d*\.?\d+))*+\])|(\[\])|[\+]|(\d*\.?\d+)$/) }
-puts "Error" if is_complex && !is_number
-puts "Is array" if is_arrays
-puts "Is string" if !is_complex && !is_arrays
-puts "Is number" if is_number
+  str.gsub!(/(plus|add|minus|multiply|divide|point|dot)/,
+            'plus' => '+',
+            'add' => '+',
+            'minus' => '-',
+            'multiply' => '*',
+            'divide' => '/',
+            'point' => '.',
+            'dot' => '.')
+  str.gsub!(/(-|\+|\*|\/|<<)/) { |s| " #{s} " }
+  tokens = str.split(' ')
+
+  is_complex = tokens.any? { |token| ['-', '*', '/'].include?(token) }
+  is_number = tokens.all? { |token| token.match(/^((\d+(\.\d+)?)|([-\+\*\/]))$/) }
+  is_arrays = tokens[0] =~ /^((\[(\d+(\.\d+)?)(,(\d+(\.\d+)?))*\])|(\[\]))$/
+  is_arrays &&= tokens.all? { |token| token.match(/^((\[(\d+(\.\d+)?)(,(\d+(\.\d+)?))*\])|(\[\])|\+|(\d+(\.\d+)?)|<<)$/) }
+  if is_complex && !is_number
+    puts "Error"
+    return nil
+  end
+
+  return eval(str) if is_arrays
+  if !is_complex && !is_arrays
+    p tokens
+    str = proccess_strings(tokens)
+    p str
+    return eval(str)
+  end
+
+  return eval(str) if is_number
+  p "Unexspected conditions"
+  nil
+end
+
+loop do
+  s = gets
+  p calc(s)
+end
